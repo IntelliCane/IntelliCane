d5c144f47dd4515e3f62ae85e2f13088f20308b8
brook_a <bk_conazole@mac.local>
2017-02-18 13:31:26 -0600

    Initial commit ( brook a. )

---
+++ app/src/main/java/intellimed/com/intellicane/MainActivity.java
@@ -0,0 +1,14 @@
+package intellimed.com.intellicane;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    // https://github.com/IntelliCane/intellimed.git
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
